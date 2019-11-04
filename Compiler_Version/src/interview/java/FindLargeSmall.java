4efaf693893e7d68431637ab5a12eefbac9f0d92
Akshay Rohankar <akrohankar@gmail.com>
2019-11-04 17:26:54 +0530

    jdbc

--- Compiler_Version/src/interview/java/FindLargeSmall.java
+++ Compiler_Version/src/interview/java/FindLargeSmall.java
@@ -3,8 +3,21 @@ package interview.java;
 public class FindLargeSmall {
 
 	public static void main(String[] args) {
+
+		int numbers[] = { 10, 2, 55, 7, 95, 45, 74, 99, 46, 100 };
 		
+		int largest = numbers[0];
+		int smallest = numbers[0];
 		
-		
+		for(int i=1;i<numbers.length;i++) {
+			
+			if(numbers[i]>largest) {
+				largest=numbers[i];
+			}
+			else if(numbers[i]<smallest) {
+				smallest=numbers[i];
+			}
+		}
+		System.out.println("Smallest "+smallest+" Largest "+largest);
 	}
 }
---
+++ Compiler_Version/src/interview/java/StringManipulation.java
@@ -0,0 +1,34 @@
+package interview.java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StringManipulation {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+
+		
+		
+		  // Create a variable for the connection string.
+		//Class.forName("com.mysql.jdbc.Driver");  
+		 
+
+        try (Connection con=DriverManager.getConnection(  
+        		"jdbc:mysql://localhost:3306/classicmodels","root","Akshay7593"); 
+        	Statement stmt = con.createStatement();) {
+            String SQL = "select DISTINCT firstname from employees;";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            // Iterate through the data in the result set and display it.
+            while (rs.next()) {
+                System.out.println(rs.getString("FirstName"));
+        }}
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+	}
+}
