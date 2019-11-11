e1669b9ee720db42821dde62f8e17958d304a2d3
Akshay Rohankar <akrohankar@gmail.com>
2019-11-11 22:20:20 +0530

    challenge added

---
+++ Compiler_Version/src/challenge/MissingDigit.java
@@ -0,0 +1,77 @@
+package challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissingDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int resultFinal = 0;
+		int digitsCount = 0;
+	
+
+		int temp = 123456789;
+
+
+		// convert int to int array.
+		List<Integer> array = new ArrayList<Integer>();
+
+		while (temp > 0) {
+			array.add(temp % 10);
+			temp /= 10;
+		}
+
+		System.out.println(array);
+		System.out.println(array.get(2));
+		
+		array.add(0, 0);
+		System.out.println(array);
+		digitsCount = array.size();
+		System.out.println(digitsCount);
+
+		resultFinal=doubleValueOperation(array,digitsCount);
+
+	}
+
+	public static int doubleValueOperation(List<Integer> array, int digitsCount) { 
+		// t--->number of test cases 
+		// numbers each containing d digits.
+																
+		int temp = 0;
+		boolean isDoubleDigit = false;
+		// List<Integer> doubleIntValues = new ArrayList<Integer>();
+		int sumArray = 0;
+
+		for (int i = 1; i < digitsCount; i++) {
+			if (i % 2 == 0) {
+				//array[i] *= 2;
+				
+				array.add(i, array.get(i)*2);
+				isDoubleDigit = (array.get(i) > 9 && array.get(i) < 100);
+
+				if (isDoubleDigit) {
+					temp += getSum(array.get(i));
+				}
+			} else {
+				sumArray += array.get(i);
+			}
+		}
+
+		return temp + sumArray;
+
+	}
+
+	static int getSum(int n) {
+		int sum = 0;
+
+		while (n != 0) {
+			sum = sum + n % 10;
+			n = n / 10;
+		}
+
+		return sum;
+	}
+
+}
